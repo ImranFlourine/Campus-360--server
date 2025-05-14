3696d137a75bbb0eae2885588eb5e33962f1bb15
IndraShikhar <shikhar1575@gmail.com>
2025-05-14 19:55:55 +0530

    add domain to cors

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ import mediaRoute from "./routes/media.route.js";
 import purchaseRoute from "./routes/purchaseCourse.route.js";
 import courseProgressRoute from "./routes/courseProgress.route.js";
 import mentorshipRoute from "./routes/mentorship.js";
-import lostItemsRouter from './routes/lostItems.js';
-import foundItemsRouter from './routes/foundItems.js';
-import emergencyRouter from './routes/emergencyRouter.js';
+import lostItemsRouter from "./routes/lostItems.js";
+import foundItemsRouter from "./routes/foundItems.js";
+import emergencyRouter from "./routes/emergencyRouter.js";
 
 dotenv.config();
 
@@ -24,12 +24,34 @@ const PORT = process.env.PORT || 8080;
 // default middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static('uploads'));
+app.use(express.static("uploads"));
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     credentials: true
+// }));
+
+////////////////////////////////////////////////////////////////
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://campus-360.netlify.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // if you're sending cookies or auth headers
+  })
+);
+////////////////////////////////////////////////////////////////
 
 // apis
 app.use("/api/v1/media", mediaRoute);
@@ -38,12 +60,10 @@ app.use("/api/v1/course", courseRoute);
 app.use("/api/v1/purchase", purchaseRoute);
 app.use("/api/v1/progress", courseProgressRoute);
 app.use("/api/mentorship", mentorshipRoute);
-app.use('/api/lost-items', lostItemsRouter);
-app.use('/api/found-items', foundItemsRouter);
-app.use('/api/emergency', emergencyRouter);
- 
+app.use("/api/lost-items", lostItemsRouter);
+app.use("/api/found-items", foundItemsRouter);
+app.use("/api/emergency", emergencyRouter);
+
 app.listen(PORT, () => {
-    console.log(`Server listen at port ${PORT}`);
+  console.log(`Server listen at port ${PORT}`);
 });
-
-
